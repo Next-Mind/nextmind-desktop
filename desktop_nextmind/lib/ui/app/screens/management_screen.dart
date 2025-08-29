c1458b292d499a31a78ab4fce40926347c76f645
marlinho <marlon@gmail.com>
2025-08-29 19:51:26 -0300

    Alterações nas telas do sistema, envolvendo as telas com o widget Scaffold

--- desktop_nextmind/lib/ui/app/screens/management_screen.dart
+++ desktop_nextmind/lib/ui/app/screens/management_screen.dart
@@ -5,6 +5,6 @@ class ManagementScreen extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return Scaffold();
   }
 }
--- desktop_nextmind/lib/ui/app/screens/reported_screen.dart
+++ desktop_nextmind/lib/ui/app/screens/reported_screen.dart
@@ -5,6 +5,6 @@ class ReportedScreen extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return Scaffold();
   }
 }
