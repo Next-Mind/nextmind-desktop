16d7a50b8dacb5c38a1652b385b6a06dbb955466
Marlon1402 <MarlonSamuel9333@gmail.com>
2025-09-09 21:23:56 -0300

    Atualização do Service, preparando para receber os dados da API, quando as rotas estiverem prontas.

--- lib/ui/app/service/dashboard_service.dart
+++ lib/ui/app/service/dashboard_service.dart
@@ -19,4 +19,33 @@ class DashboardService {
       throw Exception("Erro ao buscar $endpoint: $e");
     }
   }
+
+  // Metodo Generico Para o Retorno de Lista da API.
+  Future<List<dynamic>> fecthList(String endpoint) async {
+    try {
+      final response = await http.get(Uri.parse("$baseUrl/$endpoint"));
+      if(response.statusCode == 200){
+        final data = json.decode(response.body);
+        return data;
+      }else{
+        throw Exception("Erro ${response.statusCode} ao buscar $endpoint");
+      }
+    } catch (e) {
+      throw Exception("Erro ao buscar $endpoint: $e");
+    }
+  }
+
+  // Metodo Generico Para o Retorno das Estatisticas da API.
+  Future<Map<String, dynamic>> fecthStats(String endpoint) async {
+    try {
+      final response = await http.get(Uri.parse("$baseUrl/$endpoint"));
+      if(response.statusCode == 200){
+        return Map<String, dynamic>.from(json.decode(response.body));
+      }else{
+        throw Exception("Erro ${response.statusCode} ao buscar $endpoint");
+      }
+    } catch (e) {
+      throw Exception("Erro ao buscar $endpoint: $e");
+    }
+  }
 }
